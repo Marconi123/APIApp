ed3931328021b8090acfaacd538538a610b07566
Vladislav <vladislavgalkin5@gmail.com>
2022-08-31 03:40:26 +0300

    A new app is created

---
+++ APIApp/APIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIApp
+//
+//  Created by Владислав on 31.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
